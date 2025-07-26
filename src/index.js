6925173455b730f6bdd7996dab0d2ef1b7f98b7a
unknown <asc15.lko@gmail.com>
2025-07-26 10:10:25 +0530

    boiler plate code

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
+
+dotenv.config({
+  path: "../.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  });
