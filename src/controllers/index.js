685d4c331e87125f9d8516087d11afbf13821d14
Sourav <205398826+morningstarxcdcode@users.noreply.github.com>
2025-07-26 17:34:13 +0530

    Create index.js (#5)

---
+++ src/controllers/index.js
@@ -0,0 +1,8 @@
+// Export all controllers for easy importing
+export * from './user.controller.js';
+export * from './buyer.controller.js';
+export * from './product.controller.js';
+export * from './seller.controller.js';
+export * from './order.controller.js';
+export * from './review.controller.js';
+export * from './role.controller.js';
